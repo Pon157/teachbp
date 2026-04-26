754cc83768cc24c073fde113b0e4645b289a7864
Pon157 <Vitechek.208@gmail.com>
2026-04-27 00:01:13 +0300

    Update MessagesPage.tsx

--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { useParams, Link } from 'react-router-dom';
 import { 

c87637f0f596064230a812582c19ebf9c16ecc77
Pon157 <Vitechek.208@gmail.com>
2026-04-27 00:01:38 +0300

    Update AdminPanel.tsx

--- src/pages/AdminPanel.tsx
+++ src/pages/AdminPanel.tsx
@@ -10,7 +10,8 @@ import {
   MoreHorizontal,
   ChevronRight,
   UserCheck,
-  BookOpen
+  BookOpen,
+  Trash2
 } from 'lucide-react';
 import { motion, AnimatePresence } from 'motion/react';
 import { User, Course } from '../types';
@@ -203,12 +204,4 @@ export default function AdminPanel() {
       )}
     </div>
   );
-}
-
-function Trash2({ className }: { className?: string }) {
-    return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}>
-            <path d="M3 6h18m-2 0v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6m3 0V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2m-6 0h6"/>
-        </svg>
-    )
 }
